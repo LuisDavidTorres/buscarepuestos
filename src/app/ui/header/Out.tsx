a1bc421173efcaa758ad774a25c37649f8c3b637
LuisDavidTorres <luisdavidtt@outlook.com>
2024-07-13 21:36:45 -0400

    change text header

--- src/app/ui/header/Out.tsx
+++ src/app/ui/header/Out.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export function HeaderOut() {
   return (
     <div className="h-full">
       <header className=" bg-white p-4 flex justify-between items-center">
         <Link href="/">
-          <img
+          <Image
             src="/logo-images/logo-center-header.png"
             alt="Busca Repuestos"
-            width={260}
+            width={500}
             height={500}
-            className="hidden sm:block"
-          ></img>
+          />
           <img
             src="/logo-images/logo-start-header.png"
             alt="Busca Repuestos"
