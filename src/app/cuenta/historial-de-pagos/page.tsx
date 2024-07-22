4b0236012b224512c21e214aded92b5859913286
LuisDavidTorres <luisdavidtt@outlook.com>
2024-07-21 22:55:42 -0400

    Change header fetch payment

--- src/app/cuenta/historial-de-pagos/page.tsx
+++ src/app/cuenta/historial-de-pagos/page.tsx
@@ -4,7 +4,6 @@ import { headers } from "next/headers";
 async function loadHistory() {
   const res = await fetch(process.env.NEXT_PLUBLIC_API_URL+"/api/userHistoryPayment", {
     cache: "no-cache",
-    headers: new Headers(headers()),
   });
   const data = await res.json();
   return data;
@@ -17,7 +16,7 @@ async function Page() {
     <div className="min-h-screen bg-white p-6 rounded-md dark:text-black">
       <h1 className="font-bold text-3xl flex justify-center">Historial de Pagos</h1>
       <div className="mt-10">
-        {paymentDetails.length === undefined || paymentDetails ? (
+        {paymentDetails?.length === undefined || paymentDetails ? (
           <p className=" text-gray-500 text-base">Aun no hay pagos registrados</p>
         ) : (
           <PaymentHistorial detailPayment={paymentDetails} />
