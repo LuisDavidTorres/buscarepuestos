db41ea133c72519b3bfc11cd8e11baa7f2192888
LuisDavidTorres <luisdavidtt@outlook.com>
2024-07-22 00:09:57 -0400

    Change header fetch home page

--- src/app/cuenta/historial-de-pagos/page.tsx
+++ src/app/cuenta/historial-de-pagos/page.tsx
@@ -2,36 +2,12 @@ import { PaymentHistorial } from "@/app/ui/ListComponents/Payment-historial";
 import { headers } from "next/headers";
 
 async function loadHistory() {
-  // Obtener los encabezados de Next.js
-  const nextHeaders = headers();
-
-  // Definir el tipo de los encabezados planos
-  const plainHeaders: { [key: string]: string } = {};
-
-  // Convertir los encabezados a un objeto plano
-  nextHeaders.forEach((value, key) => {
-    plainHeaders[key] = value;
+  const res = await fetch(process.env.NEXT_PLUBLIC_API_URL+"/api/userHistoryPayment", {
+    cache: "no-cache",
+    headers: new Headers(headers()),
   });
-
-  // Verificar los encabezados convertidos
-  console.log('Plain Headers:', plainHeaders);
-
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/userHistoryPayment`, {
-      cache: "no-cache",
-      headers: plainHeaders, // Usar el objeto plano de encabezados
-    });
-
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
-    }
-
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching user history payment:', error);
-    return []; // Devolver un arreglo vacío en caso de error para evitar errores en el renderizado
-  }
+  const data = await res.json();
+  return data;
 }
 
 async function Page() {
@@ -41,8 +17,8 @@ async function Page() {
     <div className="min-h-screen bg-white p-6 rounded-md dark:text-black">
       <h1 className="font-bold text-3xl flex justify-center">Historial de Pagos</h1>
       <div className="mt-10">
-        {paymentDetails.length === 0 ? (
-          <p className="text-gray-500 text-base">Aun no hay pagos registrados</p>
+        {paymentDetails.length === undefined || paymentDetails ? (
+          <p className=" text-gray-500 text-base">Aun no hay pagos registrados</p>
         ) : (
           <PaymentHistorial detailPayment={paymentDetails} />
         )}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -36,31 +36,13 @@ async function LoadCarBrand({ email }: { email: string }) {
 }
 
 async function loadQuotes() {
-  const nextHeaders = headers();
-  const plainHeaders: { [key: string]: string } = {};
-  nextHeaders.forEach((value, key) => {
-    plainHeaders[key] = value;
+  const res = await fetch(process.env.NEXT_PLUBLIC_API_URL + "/api/quotes", {
+    method: "GET",
+    cache: "no-cache",
   });
-
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/quotes`, {
-      method: "GET",
-      cache: "no-cache",
-      headers: plainHeaders,
-    });
-
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
-    }
-
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching quotes:', error);
-    return []; // Devolver un arreglo vacío en caso de error
-  }
+  const data = await res.json();
+  return data;
 }
-
 
 async function Page() {
   const session = await getServerSession();
