86a3fa10d698b90b0dcc4d6e972f1527b530c25a
LuisDavidTorres <luisdavidtt@outlook.com>
2024-07-21 22:43:59 -0400

    Delete info payment page

--- src/app/cuenta/historial-de-pagos/page.tsx
+++ src/app/cuenta/historial-de-pagos/page.tsx
@@ -1,28 +1,15 @@
 import { PaymentHistorial } from "@/app/ui/ListComponents/Payment-historial";
 import { headers } from "next/headers";
 
-async function loadHistory() {
-  const res = await fetch(process.env.NEXT_PLUBLIC_API_URL+"/api/userHistoryPayment", {
-    method: 'GET', 
-    cache: "no-cache",
-    headers: new Headers(headers()),
-  });
-  const data = await res.json();
-  return data;
-}
+
 
 async function Page() {
-  const paymentDetails = await loadHistory();
 
   return (
     <div className="min-h-screen bg-white p-6 rounded-md dark:text-black">
       <h1 className="font-bold text-3xl flex justify-center">Historial de Pagos</h1>
       <div className="mt-10">
-        {paymentDetails.length === undefined || paymentDetails ? (
-          <p className=" text-gray-500 text-base">Aun no hay pagos registrados</p>
-        ) : (
-          <PaymentHistorial detailPayment={paymentDetails} />
-        )}
+       
       </div>
     </div>
   );
