54515ddbe30dcf541d970266f6902179af8b377e
LuisDavidTorres <luisdavidtt@outlook.com>
2024-07-24 21:10:46 -0400

    UI improvement

--- src/app/ui/header/Desing-header-down.tsx
+++ src/app/ui/header/Desing-header-down.tsx
@@ -2,7 +2,7 @@ export function DesingHeaderDown({ titulo }: { titulo: string }) {
   return (
     <>
       <div className="bg-custom-gray p-2">
-        <p className="text-center text-white">{titulo}</p>
+        <p className="text-center text-white select-none">{titulo}</p>
       </div>
       <div className="bg-custom-green w-full h-5 rounded-b-md"></div>
     </>
