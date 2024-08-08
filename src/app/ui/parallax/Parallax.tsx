dc7d449150a9ce3c9c78a217895c328306c93ba3
LuisDavidTorres <luisdavidtt@outlook.com>
2024-08-07 22:03:19 -0400

    Change blur image in page index

--- src/app/ui/parallax/Parallax.tsx
+++ src/app/ui/parallax/Parallax.tsx
@@ -15,7 +15,7 @@ export default function Parallax() {
         />
         <div className="absolute inset-0 flex justify-center items-center">
           <div className="bg-white/40 p-6 rounded-lg shadow-2xl backdrop-blur-sm max-w-md mx-4 text-center">
-            <h2 className="text-gray-700 font-bold text-3xl md:text-4xl lg:text-5xl leading-tight">
+            <h2 className="text-gray-700 font-bold max-[404px]:text-xl text-3xl md:text-4xl lg:text-5xl leading-tight">
               <span className="text-gray-700/90">Fácil,</span>
               <span className="text-gray-700/85"> Rápido</span>
               <span className="text-gray-700/80"> y Seguro</span>
