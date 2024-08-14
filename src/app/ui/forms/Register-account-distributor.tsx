7cb73eeefea954bb7b21596b62d5ce628872dae2
LuisDavidTorres <luisdavidtt@outlook.com>
2024-08-14 13:07:02 -0400

    accessibility improvement in html label

--- src/app/ui/forms/Register-account-distributor.tsx
+++ src/app/ui/forms/Register-account-distributor.tsx
@@ -187,12 +187,17 @@ export function Register_account_distribuitor() {
               </h1>
             </div>
 
-            <label htmlFor="nameCompany" className="flex flex-row space-x-1">
+            <label
+              id="label-company-name"
+              htmlFor="companyName"
+              className="flex flex-row space-x-1"
+            >
               <p className="text-red-600 ">*</p>
               <p>Nombre de Empresa</p>
             </label>
 
             <input
+              id="companyName"
               className="border-2 rounded-md px-2 w-full 2xl:w-full h-9"
               type="text"
               placeholder="Nombre"
@@ -209,12 +214,17 @@ export function Register_account_distribuitor() {
               </span>
             )}
 
-            <label htmlFor="rutCompany" className="flex flex-row space-x-1">
+            <label
+              id="label-rut-company"
+              htmlFor="rutCompany"
+              className="flex flex-row space-x-1"
+            >
               {" "}
               <p className="text-red-600 ">*</p>
               <p>Rut</p>
             </label>
             <input
+              id="rutCompany"
               className="border-2 rounded-md px-2 w-full 2xl:w-full h-9"
               type="text"
               placeholder="Rut"
@@ -244,11 +254,16 @@ export function Register_account_distribuitor() {
               </span>
             )}
 
-            <label htmlFor="businessLine" className="flex flex-row space-x-1">
+            <label
+              id="label-businessLine"
+              htmlFor="businessLine"
+              className="flex flex-row space-x-1"
+            >
               <p className="text-red-600 ">*</p>
               <p>Giro</p>
             </label>
             <input
+              id="businessLine"
               className="border-2 rounded-md px-2 w-full 2xl:w-full h-9"
               type="text"
               placeholder="Giro"
@@ -267,7 +282,11 @@ export function Register_account_distribuitor() {
 
             <div className="flex items-center space-x-1">
               {" "}
-              <label htmlFor="contactName" className="flex flex-row space-x-1">
+              <label
+                id="label-contact-name"
+                htmlFor="contactName"
+                className="flex flex-row space-x-1"
+              >
                 <p className="text-red-600 ">*</p>
                 <p>Nombre de Contacto</p>
               </label>
@@ -275,6 +294,7 @@ export function Register_account_distribuitor() {
             </div>
 
             <input
+              id="contactName"
               className="border-2 rounded-md px-2 w-full 2xl:w-full h-9"
               type="text"
               placeholder="Nombre"
@@ -293,7 +313,11 @@ export function Register_account_distribuitor() {
 
             <div className="flex items-center space-x-1">
               {" "}
-              <label htmlFor="contacPhone" className="flex flex-row space-x-1">
+              <label
+                id="label-contact-phone"
+                htmlFor="contacPhone"
+                className="flex flex-row space-x-1"
+              >
                 <p className="text-red-600 ">*</p>
                 <p>Teléfono</p>
               </label>
@@ -306,6 +330,7 @@ export function Register_account_distribuitor() {
                 clases="w-36 2xl:w-33 h-7 border-2 rounded-md h-9"
               />
               <input
+                id="contacPhone"
                 className="w-3/5 2xl:w-8/12 border-2 rounded-md px-2 h-9 [&::-webkit-inner-spin-button]:appearance-none"
                 type="number"
                 placeholder="Número"
@@ -326,12 +351,17 @@ export function Register_account_distribuitor() {
               <p className="text-red-500 text-sm">El número no es válido</p>
             )}
 
-            <label className="flex flex-row space-x-1">
+            <label
+              id="label-rubric"
+              htmlFor="rubric"
+              className="flex flex-row space-x-1"
+            >
               {" "}
               <p className="text-red-600 ">*</p>
               <p>Tipo de Repuestos</p>
             </label>
             <select
+              id="rubric"
               className="border-2 rounded-md w-full 2xl:w-full h-9"
               onChange={(e) => setRubric(e.target.value)}
               required
@@ -350,7 +380,11 @@ export function Register_account_distribuitor() {
               </option>
             </select>
 
-            <label className="flex flex-row space-x-1">
+            <label
+              id="label-carbrands"
+              htmlFor="carBrands2"
+              className="flex flex-row space-x-1"
+            >
               {" "}
               <p className="text-red-600 ">*</p>
               <p>Marcas que Comercializa</p>
@@ -371,8 +405,13 @@ export function Register_account_distribuitor() {
                 required
               />
             </div>
-            <label className="inline-flex items-center cursor-pointer">
+            <label
+              id="laber-all-car"
+              htmlFor="allCars"
+              className="inline-flex items-center cursor-pointer"
+            >
               <input
+                id="allCars"
                 type="checkbox"
                 value=""
                 checked={selectAll}
@@ -391,13 +430,18 @@ export function Register_account_distribuitor() {
             </h1>
             <p className="text-xs">(Crea tu Cuenta BR)</p>
             <div className="flex items-center space-x-1">
-              <label htmlFor="email" className="flex flex-row space-x-1">
+              <label
+                id="label-email"
+                htmlFor="email"
+                className="flex flex-row space-x-1"
+              >
                 <p className="text-red-600 ">*</p>
                 <p>Email</p>
               </label>
               <p className="text-xs">(Empresa)</p>
             </div>
             <input
+              id="email"
               className="border-2 rounded-md px-2 w-full 2xl:w-full h-9"
               type="email"
               placeholder="Email"
@@ -418,13 +462,18 @@ export function Register_account_distribuitor() {
                 {errors.email.message?.toString()}
               </span>
             )}
-            <label className="flex flex-row space-x-1">
+            <label
+              id="label-password"
+              htmlFor="password"
+              className="flex flex-row space-x-1"
+            >
               <p className="text-red-600 ">*</p>
               <p>Contraseña</p>
             </label>
             <div className="relative w-full">
               {" "}
               <input
+                id="password"
                 className="border-2 rounded-md px-2 w-full 2xl:w-full h-9"
                 type={showPassword ? "text" : "password"}
                 placeholder="Contraseña"
@@ -467,11 +516,13 @@ export function Register_account_distribuitor() {
               required
             ></input>
             <label
-              htmlFor="terminos"
+              id="label-terms"
+              htmlFor="terms"
               className="flex flex-col space-y-1 sm:flex-row sm:space-x-1 sm:space-y-0"
             >
               <p className="inline text-sm">Declara aceptar nuestros</p>
               <a
+                id="terms"
                 href="/terminos"
                 target="_blank"
                 className="no-underline hover:underline text-blue-700 inline text-sm "
