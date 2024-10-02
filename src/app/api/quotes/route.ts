3beb67618889c91c40e91b4deef203f2df0169f8
LuisDavidTorres <luisdavidtt@outlook.com>
2024-10-02 20:03:04 -0300

    bug fix that did not show quotes

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -69,7 +69,7 @@ export async function GET() {
     const quotes = await prisma.quotation.findMany({
       where: {
         clicks: {
-          lt: 0,
+          lt: 3,
         },
         carBrand: {
           in: includeCardBrands,
